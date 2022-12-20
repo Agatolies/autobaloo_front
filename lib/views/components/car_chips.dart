1413e3f3670c6549c046039f0c5739277d64b4c0
Agatolies <laure.deste@gmail.com>
2022-12-20 18:00:25 +0100

    Update car_chips.dart

--- lib/views/components/car_chips.dart
+++ lib/views/components/car_chips.dart
@@ -14,26 +14,26 @@ class CarChips extends StatelessWidget {
       children: [
         Chip(
           avatar: const Icon(Icons.directions_car, color: Colors.lightBlue),
-          label: Text('${car.carKilometers} km'),
+          label: Text('${car.carKilometers} km', textScaleFactor: 0.8),
           labelPadding: const EdgeInsets.symmetric(horizontal: 8),
         ),
         if (car.carModel?.modelFuelType != null)
           Chip(
             avatar:
                 const Icon(Icons.local_gas_station, color: Colors.lightGreen),
-            label: Text(car.carModel!.modelFuelType),
+            label: Text(car.carModel!.modelFuelType, textScaleFactor: 0.8),
             labelPadding: const EdgeInsets.symmetric(horizontal: 8),
           ),
         if (car.carModel?.modelEngineBox != null)
           Chip(
             avatar: const Icon(Icons.build, color: Colors.orange),
-            label: Text(car.carModel!.modelEngineBox),
+            label: Text(car.carModel!.modelEngineBox, textScaleFactor: 0.8),
             labelPadding: const EdgeInsets.symmetric(horizontal: 8),
           ),
         // if (car.carModel?.modelEuropeanCode != null)
         //   Chip(
         //     avatar: const Icon(Icons.info, color: Colors.pink),
-        //     label: Text(car.carModel!.modelEuropeanCode),
+        //     label: Text(car.carModel!.modelEuropeanCode, textScaleFactor: 0.8),
         //     labelPadding: const EdgeInsets.symmetric(horizontal: 8),
         //   ),
       ],
