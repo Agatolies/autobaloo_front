c9a385c95ef3d1cc325be990d57aa45740d1bb06
Agatolies <laure.deste@gmail.com>
2022-12-21 16:13:52 +0100

    Create title_section.dart

---
+++ lib/views/sections/title_section.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TitleSection extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final Widget? leading;
+  final Widget? trailing;
+
+  const TitleSection({
+    super.key,
+    required this.title,
+    this.subtitle,
+    this.leading,
+    this.trailing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16),
+      child: Row(
+        children: [
+          if (leading != null) leading!,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+                if (subtitle != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Column(
+                      children: [
+                        Divider(color: Colors.grey[300]),
+                        Text(
+                          subtitle!,
+                          style: Theme.of(context).textTheme.subtitle2,
+                        ),
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          if (trailing != null) trailing!,
+        ],
+      ),
+    );
+  }
+}
